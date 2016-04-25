71f737c82fbb913c19e394f3c00e8de21714e55b
Mykola Prokop <mykolaprokop@mail.com>
2016-04-25 01:51:12 -0500

    <heroku> heorku database for production to englandkit

--- schooldatabase.sql
+++ schooldatabase.sql
@@ -2,8 +2,8 @@
 --
 -- Host: localhost    Database: twitter_school_development
 -- ------------------------------------------------------
--- Server version	5.6.27
-use database twitter_school_development;
+-- -- Server version	5.6.27
+-- use database twitter_school_development;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
